d99bfd406fe6bc035e70b36058fcec49f51fc21b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-07 16:21:12 +0200

    Initial commit

---
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Entry Screen - Awesome!</Text>
+    </View>
+  );
+}
+
+
