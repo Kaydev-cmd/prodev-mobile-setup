16c542551b7036b839c29fb45374d24d2903756b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-08 11:58:15 +0200

    feat: implement sign-in screen layout with safe area, navigation header, and intro text

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { styles } from "@/styles";
+import { Ionicons } from "@expo/vector-icons";
+import { Image, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.navGroup}>
+          <Ionicons name="arrow-back" size={25} />
+          <Image source={require("@/assets/images/logo.png")} />
+        </View>
+        <Text style={styles.largeText}>Sign in to your</Text>
+        <Text style={styles.largeText}>Account</Text>
+        <Text style={styles.smallText}>
+          Enter your email address and password to sign in.
+        </Text>
+
+        {/* Form Group */}
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
