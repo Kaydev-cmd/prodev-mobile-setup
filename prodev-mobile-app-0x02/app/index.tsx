d934aa1e2a14c52e2062ce2583bba902c77bce33
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-08 10:33:09 +0200

    feat: add call-to-action button group with Join, SSign In, and Continue options

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -4,6 +4,7 @@ import {
   ImageBackground,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
@@ -16,15 +17,35 @@ export default function Index() {
           source={require("@/assets/images/backgroundImage.png")}
           style={styles.background}
           resizeMode="cover"
-        />
-        <View style={styles.companyLogo}>
-          <Image source={require("@/assets/images/Logo.png")} />
-        </View>
-        <View style={styles.textGroup}>
-          <Text style={styles.textLarge}>Find your favorite place here</Text>
-          <Text style={styles.textSmall}>The best prices for over 2</Text>
-          <Text style={styles.textSmall}>million properties worldwide</Text>
-        </View>
+        >
+          <View style={styles.container}>
+            <View style={styles.companyLogo}>
+              <Image source={require("@/assets/images/Logo.png")} />
+            </View>
+            <View style={styles.textGroup}>
+              <Text style={styles.textLarge}>
+                Find your favorite place here
+              </Text>
+              <Text style={styles.textSmall}>The best prices for over 2</Text>
+              <Text style={styles.textSmall}>million properties worldwide</Text>
+            </View>
+            <View style={styles.buttonGroup}>
+              <TouchableOpacity style={styles.button}>
+                <Text style={{ ...styles.textSmall, color: "black" }}>
+                  Join Here
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.transparentButton}>
+                <Text style={styles.textSmall}>Sign In</Text>
+              </TouchableOpacity>
+
+              <View style={{ alignItems: "center", paddingVertical: 20 }}>
+                <Text style={{ color: "white" }}>Continue to home</Text>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -33,6 +54,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "space-around",
+    paddingVertical: 20,
   },
   background: {
     flex: 1,
@@ -61,5 +84,29 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "400",
     textAlign: "center",
+  },
+  transparentButton: {
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 20,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    fontSize: 20,
+  },
+  button: {
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 20,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    fontSize: 20,
+    backgroundColor: "white",
+  },
+  buttonGroup: {
+    flexDirection: "column",
+    gap: 20,
+    paddingHorizontal: 20,
   },
 });
