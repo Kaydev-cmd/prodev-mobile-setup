48c6638854d73a5d6f7e5494663ea3c4d5a2654d
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-08 09:23:56 +0200

    feat: add full-screen background image using ImageBackground with responsive styling

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -1,11 +1,28 @@
+import { Dimensions, ImageBackground, StyleSheet } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        
+        <ImageBackground
+          source={require("@/assets/images/backgroundImage.png")}
+          style={styles.background}
+          resizeMode="cover"
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+    height: Dimensions.get("window").height,
+  },
+});
